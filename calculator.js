b581865fc777ef3e0f9eb0badee9f3301474c2f1
aaftab-ahmed05 <aaftab2005ahmed@gmail.com>
2025-05-27 22:05:01 +0530

    calculator.js

--- calculator.js
+++ calculator.js
@@ -1,9 +1,14 @@
+let flag = false;
 function insert(str) {
+    if (flag){
+        clearall()
+    }
     let d = document.getElementById("disp")
     if(str != '=')
     d.value += str;
     else {
         d.value = eval(d.value);
+        flag = true;
     }
 }
 
